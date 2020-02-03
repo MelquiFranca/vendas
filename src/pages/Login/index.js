3e0c2737d88c3c6619ea0bb5044d2472a7025cce
Melqui Franca <melquisedeque.pereira@contraprova.com.br>
2020-02-03 16:22:03 -0300

    Tela de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css';
 import imagemBackground from '../../images/backgroundDolar.png';
 
@@ -9,7 +10,27 @@ export default function Login() {
             className="corpo"
             style={{backgroundImage: `url(${imagemBackground})`, backgroundSize: 'contain'}}
         >
-            Login
+            
+            <div className="formulario-login">
+                <div className="logo">
+                    <FontAwesomeIcon icon={faCartArrowDown} size="2x" />
+                    <span>Soft_Vendas</span>
+                </div>
+                <form action="">
+                    <input 
+                        type="text" 
+                        className="form-input"
+                        placeholder="E-mail"
+                    />
+                    <input 
+                        type="text" 
+                        className="form-input"
+                        placeholder="Senha"
+                    />
+
+                    <button type="submit" className="btn-login btn-confirma">Entrar</button>
+                </form>
+            </div>
         </div>
     )
 }
